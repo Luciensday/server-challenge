44bd628cd3ae69a6999d4f932c104eae8ee3c0bd
Lucien Lin <luciensday@gmail.com>
2024-01-21 11:27:04 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const server = express();
+
+// function logger(request, response, next) {
+//   console.log(request.method + " " + request.url);
+//   next();
+}
+
+// server.use(logger);
+
+server.get("/", (request, response) => {
+  response.send(`
+    <!doctype html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>Home</title>
+        <link rel="stylesheet" href="/style.css">
+      </head>
+      <body>
+        <h1>Hello Express</h1>
+      </body>
+    </html>
+    
+    `);
+});
+
+module.export = server;
